4a5b4060d3ebc9829acb2dc49f14828883e21656
amlp33 <sharmarahul965797@gmail.com>
2022-09-20 16:44:57 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,12 +16,13 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
     FormsModule,
     AngularFireModule.initializeApp({
-      apiKey: "xxxxxxxxxxxxxxxxxxxxxxx",
-      authDomain: "js-scrambler-demo-app.firebaseapp.com",
-      projectId: "js-scrambler-demo-app",
-      storageBucket: "js-scrambler-demo-app.appspot.com",
-      messagingSenderId: "xxxxxxxxxxxx",
-      appId: "xxxxxxxxxxxxxxxxxxxxxxx"
+      apiKey: "AIzaSyBskiJYa7Mtf5FBXt36ZR6Jffyex21kkcc",
+      authDomain: "angular-blog-fa29f.firebaseapp.com",
+      projectId: "angular-blog-fa29f",
+      storageBucket: "angular-blog-fa29f.appspot.com",
+      messagingSenderId: "105993470880",
+      appId: "1:105993470880:web:1bf81027f5d39f61fd1ac5"
+
     }),
     AngularFirestoreModule
   ],
